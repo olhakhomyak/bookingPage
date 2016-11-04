d883006a8460464fecd2577677114e5c30a0155a
Olha Khomyak <khomyak.olha@gmail.com>
2016-11-04 12:56:27 +0200

    upload new project

---
+++ database/migrations/2016_11_03_122026_create_table_reservation.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableReservation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation', function (Blueprint $table){
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('room_id');
+            $table->time('reserv_from');
+            $table->time('reserv_to');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('room_id')->references('id')->on('rooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservation');
+    }
+}
